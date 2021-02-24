72a25e553feb966844f44d0356dbfcdd54cb4f83
Joona Oja <n0ojjo00@students.oamk.fi>
2021-02-24 15:24:04 +0200

    todolist assignment done

--- retrieve.php
+++ retrieve.php
@@ -1,11 +1,15 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
+require_once "inc/headers.php";
+require_once "inc/functions.php";
 
-$db = new PDO("mysql:host=localhost;dbname=todo;charset=utf8","root","");
-$sql = "select * from task";
-$query = $db->query($sql);
-$results = $query->fetchAll(PDO::FETCH_ASSOC);
-
-header("HTTP/1.1 200 OK");
-echo json_encode($results);
+try {
+    $db = openDB();
+    $sql = "select * from task";
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    header("HTTP/1.1 200 OK");
+    echo json_encode($results);
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
