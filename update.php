e81a762c0389b405894aa4f50b56f0134e61f752
Joona Oja <n0ojjo00@students.oamk.fi>
2021-03-03 10:13:41 +0200

    valmis toistaiseksi

---
+++ update.php
@@ -0,0 +1,24 @@
+<?php
+require_once "inc/headers.php";
+require_once "inc/functions.php";
+
+$input = json_decode(file_get_contents("php://input"));
+
+$id = filter_var($input->id,FILTER_SANITIZE_NUMBER_INT);
+$description = filter_var($input->description,FILTER_SANITIZE_STRING);
+
+try {
+    $db = openDB();
+
+    $query = $db->prepare("update task set description=:description where id=:id");
+    $query->bindValue(":description",$description,PDO::PARAM_STR);
+    $query->bindValue(":id",$id,PDO::PARAM_INT);
+    $query->execute();
+
+    header("HTTP/1.1 200 OK");
+    $data = array("id" => $id, "description" => $description);
+    echo json_encode($data);
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
